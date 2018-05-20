2fdee174eb0b8da6e7914caa0017d2a6454a6586
Jannis Mattheis <contact@jmattheis.de>
2018-05-20 21:13:53 +0200

    Add navigation for Home - Messages

--- src/AuthLoading.js
+++ src/AuthLoading.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import {
-    ActivityIndicator,
-    AsyncStorage,
-    StatusBar,
-    StyleSheet,
-    View,
-} from 'react-native';
+import {ActivityIndicator, StatusBar, View} from 'react-native';
 import SharedPreferences from 'react-native-shared-preferences';
 
 export default class AuthLoadingScreen extends React.Component {
     componentDidMount() {
         SharedPreferences.getItem('@global:token', (token) => {
-            this.props.navigation.navigate(token ? 'Messages' : 'Login');
+            this.props.navigation.navigate(token ? 'MainNavigation' : 'Login');
         });
     }
 
     render() {
         return (
-            <View >
-                <ActivityIndicator />
-                <StatusBar barStyle="default" />
+            <View>
+                <ActivityIndicator/>
+                <StatusBar barStyle="default"/>
             </View>
         );
     }
