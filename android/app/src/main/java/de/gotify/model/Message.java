4bcf174ba36887c1e3b84dc2e7e3c6273aa56c44
Jannis Mattheis <contact@jmattheis.de>
2018-05-22 18:27:48 +0200

    Add model structures

---
+++ android/app/src/main/java/de/gotify/model/Message.java
@@ -0,0 +1,40 @@
+package de.gotify.model;
+
+public class Message {
+    private Integer id;
+    private String title;
+    private String message;
+    private int priority;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
---
+++ android/app/src/main/java/de/gotify/model/Paging.java
@@ -0,0 +1,13 @@
+package de.gotify.model;
+
+public class Paging {
+    private int since;
+
+    public int getSince() {
+        return since;
+    }
+
+    public void setSince(int since) {
+        this.since = since;
+    }
+}
