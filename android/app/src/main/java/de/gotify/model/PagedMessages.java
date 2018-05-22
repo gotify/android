9652151061708700b8a1c294f10aafd0cd70276d
Jannis Mattheis <contact@jmattheis.de>
2018-05-22 18:27:48 +0200

    Add model structures

---
+++ android/app/src/main/java/de/gotify/model/PagedMessages.java
@@ -0,0 +1,24 @@
+package de.gotify.model;
+
+import java.util.List;
+
+public class PagedMessages {
+    private Paging paging;
+    private List<Message> messages;
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public Paging getPaging() {
+        return paging;
+    }
+
+    public void setPaging(Paging paging) {
+        this.paging = paging;
+    }
+}
