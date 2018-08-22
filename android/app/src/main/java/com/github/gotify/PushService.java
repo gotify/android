01ca410aae16da3c049f41aef48486183a08270b
Jannis Mattheis <contact@jmattheis.de>
2018-08-22 19:29:29 +0200

    Start service sticky

--- android/app/src/main/java/com/github/gotify/PushService.java
+++ android/app/src/main/java/com/github/gotify/PushService.java
@@ -99,7 +99,7 @@ public class PushService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        return START_REDELIVER_INTENT;
+        return START_STICKY;
     }
 
     @Override
