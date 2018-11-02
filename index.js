44df7cd17288e1bcbf35fba7d1ab5393bb4c4ec8
Jannis Mattheis <contact@jmattheis.de>
2018-11-02 13:10:07 +0100

    Remove old project

--- index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './App';
-
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
-AppRegistry.registerComponent('gotify', () => App);
