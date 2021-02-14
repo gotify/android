e7b376a501b3436f7624f727584373612c2c081a
Jannis Mattheis <contact@jmattheis.de>
2021-02-14 10:49:59 +0100

    Use table aware movement plugin
    Fixes #136

--- app/src/main/java/com/github/gotify/messages/ListMessageAdapter.java
+++ app/src/main/java/com/github/gotify/messages/ListMessageAdapter.java
@@ -26,6 +26,7 @@ import com.github.gotify.messages.provider.MessageWithImage;
 import com.squareup.picasso.Picasso;
 import io.noties.markwon.Markwon;
 import io.noties.markwon.core.CorePlugin;
+import io.noties.markwon.ext.tables.TableAwareMovementMethod;
 import io.noties.markwon.ext.tables.TablePlugin;
 import io.noties.markwon.image.picasso.PicassoImagesPlugin;
 import io.noties.markwon.movement.MovementMethodPlugin;
@@ -62,7 +63,7 @@ public class ListMessageAdapter extends RecyclerView.Adapter<ListMessageAdapter.
         this.markwon =
                 Markwon.builder(context)
                         .usePlugin(CorePlugin.create())
-                        .usePlugin(MovementMethodPlugin.create())
+                        .usePlugin(MovementMethodPlugin.create(TableAwareMovementMethod.create()))
                         .usePlugin(PicassoImagesPlugin.create(picasso))
                         .usePlugin(TablePlugin.create(context))
                         .build();
