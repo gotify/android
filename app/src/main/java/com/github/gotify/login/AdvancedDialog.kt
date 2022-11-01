fadcfc0bfcb398d169e7d7c6013501899bd05163
Niko Diamadis <niko@cyb3rko.de>
2022-11-01 23:10:34 +0100

    Rewrite 'login' to Kotlin

---
+++ app/src/main/java/com/github/gotify/login/AdvancedDialog.kt
@@ -0,0 +1,67 @@
+package com.github.gotify.login
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.CompoundButton
+import com.github.gotify.R
+import com.github.gotify.databinding.AdvancedSettingsDialogBinding
+
+internal class AdvancedDialog(
+    private val context: Context,
+    private val layoutInflater: LayoutInflater
+) {
+    private lateinit var binding: AdvancedSettingsDialogBinding
+    private var onCheckedChangeListener: CompoundButton.OnCheckedChangeListener? = null
+    private lateinit var onClickSelectCaCertificate: Runnable
+    private lateinit var onClickRemoveCaCertificate: Runnable
+
+    fun onDisableSSLChanged(
+        onCheckedChangeListener: CompoundButton.OnCheckedChangeListener?
+    ): AdvancedDialog {
+        this.onCheckedChangeListener = onCheckedChangeListener
+        return this
+    }
+
+    fun onClickSelectCaCertificate(onClickSelectCaCertificate: Runnable): AdvancedDialog {
+        this.onClickSelectCaCertificate = onClickSelectCaCertificate
+        return this
+    }
+
+    fun onClickRemoveCaCertificate(onClickRemoveCaCertificate: Runnable): AdvancedDialog {
+        this.onClickRemoveCaCertificate = onClickRemoveCaCertificate
+        return this
+    }
+
+    fun show(disableSSL: Boolean, selectedCertificate: String?): AdvancedDialog {
+        binding = AdvancedSettingsDialogBinding.inflate(layoutInflater)
+        binding.disableSSL.isChecked = disableSSL
+        binding.disableSSL.setOnCheckedChangeListener(onCheckedChangeListener)
+        if (selectedCertificate == null) {
+            showSelectCACertificate()
+        } else {
+            showRemoveCACertificate(selectedCertificate)
+        }
+        AlertDialog.Builder(context)
+            .setView(binding.root)
+            .setTitle(R.string.advanced_settings)
+            .setPositiveButton(context.getString(R.string.done), null)
+            .show()
+        return this
+    }
+
+    private fun showSelectCACertificate() {
+        binding.toggleCaCert.setText(R.string.select_ca_certificate)
+        binding.toggleCaCert.setOnClickListener { onClickSelectCaCertificate.run() }
+        binding.selecetedCaCert.setText(R.string.no_certificate_selected)
+    }
+
+    fun showRemoveCACertificate(certificate: String) {
+        binding.toggleCaCert.setText(R.string.remove_ca_certificate)
+        binding.toggleCaCert.setOnClickListener {
+            showSelectCACertificate()
+            onClickRemoveCaCertificate.run()
+        }
+        binding.selecetedCaCert.text = certificate
+    }
+}
