21383fba1b25dcb1e3e0a2530be6132d6136c34b
Jannis Mattheis <contact@jmattheis.de>
2023-02-06 13:13:45 +0100

    Use data class

--- app/src/main/kotlin/com/github/gotify/messages/provider/MessageWithImage.kt
+++ app/src/main/kotlin/com/github/gotify/messages/provider/MessageWithImage.kt
@@ -2,7 +2,7 @@ package com.github.gotify.messages.provider
 
 import com.github.gotify.client.model.Message
 
-internal class MessageWithImage {
-    lateinit var message: Message
-    var image: String? = null
-}
+internal data class MessageWithImage(
+    val message: Message,
+    val image: String?
+)
