c32c34cd026ec7da05e51780c9021ffc1b6e2ae1
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-09-30 23:52:41 +0200

    Remove unneeded classes

--- app/src/main/kotlin/com/github/gotify/messages/provider/MessageDeletion.kt
+++
@@ -1,9 +0,0 @@
-package com.github.gotify.messages.provider
-
-import com.github.gotify.client.model.Message
-
-internal class MessageDeletion(
-    val message: Message,
-    val allPosition: Int,
-    val appPosition: Int
-)
